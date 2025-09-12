fbda645a55d93cf92d6eff221f63f414d389c431
rajeshdumpeti <dumpetirajesh123@gmail.com>
2025-09-12 19:20:02 -0400

    Hamburger fix for th relase notes

--- src/features/release-notes/ReleaseNotesView.jsx
+++ src/features/release-notes/ReleaseNotesView.jsx
@@ -155,7 +155,7 @@ const ReleaseNotesView = () => {
     return (
         <div className="h-screen w-full flex flex-col bg-gray-50">
             <div className="w-full bg-gray-900">
-                <Header showHamburger={false} showTitle />
+                <Header showHamburger onToggleSidebar={() => setIsSidebarOpen((s) => !s)} showTitle />
             </div>
 
             <div className="flex-1 min-h-0 w-full">
